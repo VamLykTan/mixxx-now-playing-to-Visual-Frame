f621841b7f290e9bec23b36b17958fadfb187f17
Maik Geßner <maik81SE@yahoo.de>
2024-04-02 15:53:00 +0200

    Add files via upload

--- Scource/main.pas
+++ Scource/main.pas
@@ -43,6 +43,7 @@ type
   TTrack_ID                            = class(TForm)
     BcLabel1                           : TBcLabel;
     Texte1, Texte2                     : TLabel;
+    BCTimer: TTimer;
     TrackTime                          : TTimer;
 
     procedure FormCreate(Sender: TObject);
@@ -180,7 +181,15 @@ end;
 
 procedure TTrack_ID.BcTimer_Run(sender :tobject);
 begin
-
+  BCLabel1.Background.Gradient1.Point1xPercent := _2X;
+  if up = False then
+     if _2X > 0 then
+       dec(_2X)
+     else Up                        := True;
+  if up = False then
+     if _2X > 0 then
+        dec(_2X)
+     else Up                        := True;
 end;
 
 procedure TTrack_ID.TrackTime_run(Sender: TObject);
@@ -189,84 +198,94 @@ begin
   if     Manuell then
      Mixxx_File                         := MTrackFile;
   if not Manuell then begin
-    if FileExists(TrackFile) then
-      Mixxx_File                        := TrackFile;
-//    if FileExists(TrackFile[0]) then
-//      Mixxx_File                       := TrackFile[0];
-//    if FileExists(TrackFile[1]) then
-//      Mixxx_File                       := TrackFile[1];
+    if FileExists(TrackFile[0]) then
+      Mixxx_File                       := TrackFile[0];
+    if FileExists(TrackFile[1]) then
+      Mixxx_File                       := TrackFile[1];
+    if FileExists(TrackFile[2]) then
+      Mixxx_File                       := TrackFile[2];
     S.LoadFromFile(Mixxx_File);
-    if S[0] <> temp then begin
-      Cue_Load                               := False;
-      temp                                   := S[0];
-      end;
-{    if Not Track_List then begin
-      zzz                                    := 0;
-      ss                                     := 0;
-      mm                                     := 0;
-      Index                                  := 0;
-      Track_List                             := True;
-      end;
-    if S[0] = 'DJ VAMLYKTAN - ROCTRONICS @ SUNDAY           ' then
-      if NOT Cue_Load then begin
-        TrackList.LoadCUE_Data('/run/user/1000/gvfs/smb-share:server=dj-rechner.local,share=musik/Mixxx/Recordings/VamLykTan/Mixe 2023/Live-Mix/Roctronics @ Sunday.cue');
-        Track_List                           := True;
+//    Track_ID.Caption                   := 'S.LoadFromFile(Mixxx_File)';
+    try
+      if S[0] <> temp then begin
+        Cue_Load                               := False;
+        temp                                   := S[0];
         end;
-    if S[0] = 'VAMLYKTAN - SPAIN-LATEIN OF METAL & ELECTRO           ' then
-      if NOT Cue_Load then begin
-        TrackList.LoadCUE_Data('/run/user/1000/gvfs/smb-share:server=dj-rechner.local,share=musik/Mixxx/Recordings/VamLykTan/Mixe 2022/Spain-Latein of Metal & Electro.cue');
-        Track_List                           := True;
-        end;     }
-    if S.Count <> 0 then begin
-      BCLabel1.Visible               := True;
-{      if Cue_Load then begin
-          inc(zzz);
-          if zzz = 100 then begin
-            zzz                        := 0;
-            inc(ss);
-            if ss = 60 then begin
-              ss                       := 0;
-              inc(mm);
-              end;
-            end;
-          case zzz of
-            0..9: begin
-                  _zzz                 := '0' + IntToStr(zzz);
+      if Not Track_List then begin
+        zzz                                    := 0;
+        ss                                     := 0;
+        mm                                     := 0;
+        Index                                  := 0;
+        Track_List                             := True;
+        end;
+      if S[0] = 'DJ VAMLYKTAN - ROCTRONICS @ SUNDAY           ' then
+        if NOT Cue_Load then begin
+          TrackList.LoadCUE_Data('/run/user/1000/gvfs/smb-share:server=dj-rechner.local,share=musik/Mixxx/Recordings/VamLykTan/Mixe 2023/Live-Mix/Roctronics @ Sunday.cue');
+          Track_List                           := True;
+          end;
+      if S[0] = 'VAMLYKTAN - SPAIN-LATEIN OF METAL & ELECTRO           ' then
+        if NOT Cue_Load then begin
+          TrackList.LoadCUE_Data('/run/user/1000/gvfs/smb-share:server=dj-rechner.local,share=musik/Mixxx/Recordings/VamLykTan/Mixe 2022/Spain-Latein of Metal & Electro.cue');
+          Track_List                           := True;
+          end;
+        if S.Capacity <> 0 then begin
+        BCLabel1.Visible               := True;
+        Track_ID.Caption                   := 'BCLabel1.Visible:=True;';
+{          if Cue_Load then begin
+        inc(zzz);
+        if zzz = 100 then begin
+          zzz                        := 0;
+          inc(ss);
+          end;
+        if ss = 60 then begin
+          ss                       := 0;
+          inc(mm);
+          end;
+        case zzz of
+          0..9: begin
+                _zzz                 := '0' + IntToStr(zzz);
+                end;
+          10..99: begin
+                  _zzz               := IntToStr(zzz);
                   end;
-            10..99: begin
-                    _zzz               := IntToStr(zzz);
-                    end;
-            end;
-          case ss of
-            0..9: begin
-                  _ss                  := '0' + IntToStr(ss);
+          end;
+        case ss of
+          0..9: begin
+                _ss                  := '0' + IntToStr(ss);
+                end;
+          10..59: begin
+                  _ss                := IntToStr(ss);
                   end;
-            10..59: begin
-                    _ss                := IntToStr(ss);
-                    end;
-            end;
-          case mm of
-            0..9: begin
-                  _mm                  := '0' + IntToStr(mm);
-                  end;
-            else _mm                   := IntToStr(mm);
-            end;
+          end;
+        case mm of
+          0..9: begin
+                _mm                  := '0' + IntToStr(mm);
+                end;
+          else _mm                   := IntToStr(mm);
+          end;
 //          Track_ID.Caption             := _mm + ':' + _ss + ':' + _zzz;
-          if TrackList.Cue.Time[index] = _mm + ':' + _ss + ':' + _zzz then begin
-          Caption          := TrackList.Cue.Artist[Index] + #10#13 + TrackList.Cue.Song[index];
-          Track_ID.Caption := _mm + ':' + _ss + ':' + _zzz;
+        if TrackList.Cue.Time[index] = _mm + ':' + _ss + ':' + _zzz then begin
+          Texte2.Caption                     := TrackList.Cue.Artist[Index] + #10#13 + TrackList.Cue.Song[index];
+          Track_ID.Caption                   := _mm + ':' + _ss + ':' + _zzz;
           inc(index);
           end;
-          end else }
-        Texte2.Caption                        := Umlaut_Kill(temp);
-      Texte2.Left                           := (BCLabel1.Width - Texte2.Width) div 2;
-      Texte2.Top                            := (BCLabel1.Top + Texte2.Height) div 2;
-      Texte2.Alignment                      := taCenter;
+        end
+        else    }
+          Texte2.Caption                     := Umlaut_Kill(temp);
+          Track_ID.Caption                   := 'Umlaut_Kill(temp)';
+      Texte2.Left                            := (BCLabel1.Width - Texte2.Width) div 2;
+      Track_ID.Caption                       := '(BCLabel1.Width - Texte2.Width) div 2';
+      Texte2.Top                             := (BCLabel1.Top + Texte2.Height) div 2;
+      Track_ID.Caption                       := '(BCLabel1.Top + Texte2.Height) div 2';
+      Texte2.Alignment                       := taCenter;
+      Track_ID.Caption                       := 'taCenter';
       end
       else begin
         BCLabel1.Visible               := False;
         Texte2.Caption                 := Umlaut_Kill('ERROR Enter EXIT - File Not Found');
         end;
+      Finally
+      end;
     end;
   {$endif}
 end;
@@ -280,7 +299,8 @@ begin
   WaitForTitle                                   := RTLEventCreate;
   while not Application.Terminated do begin
     RtlEventWaitFor(WaitForTitle);
-    Track_ID.Caption                             := 'BCLabel wartet, bis Titel ausgefüht wurde';
+    Track_ID.Caption                             := 'BCLabel wartet, bis Track_ID ausgefüht wurde';
+//    Track_ID.BCTimer.Enabled                     := True;
 //    Track_ID.TrackTimer.Enabled                  := True;
     end;
 end;
@@ -302,10 +322,15 @@ begin
         if FileExists(TrackFile[2]) then
           Mixxx_File                       := TrackFile[2];
         S.LoadFromFile(Mixxx_File);
+        Track_ID.Caption                   := 'S.LoadFromFile(Mixxx_File)';
         if S[0] <> temp then begin
+          Track_ID.Caption                       := 'if S[0] <> temp then begin';
           Cue_Load                               := False;
+          Track_ID.Caption                       := 'Cue_Load:= False;';
           temp                                   := S[0];
+          Track_ID.Caption                       := 'temp:= S[0];';
           end;
+        Track_ID.Caption                   := '320';
   {      if Not Track_List then begin
           zzz                                    := 0;
           ss                                     := 0;
@@ -324,8 +349,10 @@ begin
             Track_List                           := True;
             end;     }
         if S.Count <> 0 then begin
+          Track_ID.Caption                   := 'if S.Count <> 0 then begin';
           BCLabel1.Visible               := True;
-          if Cue_Load then begin
+          Track_ID.Caption                   := 'BCLabel1.Visible:=True;';
+{          if Cue_Load then begin
             inc(zzz);
             if zzz = 100 then begin
               zzz                        := 0;
@@ -364,11 +391,15 @@ begin
               inc(index);
               end;
             end
-            else
+            else    }
               Texte2.Caption                     := Umlaut_Kill(temp);
+              Track_ID.Caption                   := '383';
           Texte2.Left                            := (BCLabel1.Width - Texte2.Width) div 2;
+          Track_ID.Caption                       := '385';
           Texte2.Top                             := (BCLabel1.Top + Texte2.Height) div 2;
+          Track_ID.Caption                       := '387';
           Texte2.Alignment                       := taCenter;
+          Track_ID.Caption                       := '389';
           end
           else begin
             BCLabel1.Visible               := False;
