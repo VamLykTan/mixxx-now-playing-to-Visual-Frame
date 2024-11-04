62c3490842fa75895b3c83c79cb23ffc912d0748
Maik Geßner <maik81SE@yahoo.de>
2024-11-04 18:44:38 +0100

    Create mixxx-now-playing-4-windows.bat

---
+++ Scource/mnp4w/mixxx-now-playing-4-windows.bat
@@ -0,0 +1,2 @@
+# Windows-Version zum Linuxprogramm "mixxx-now-playing" von https://github.com/ViktorNova/mixxx-now-playing
+# Diese Tool wurde erstellt von Dark-Zero-Radio, vertreten durch "Paul Paradox"
